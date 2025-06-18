9e0ebb36bbfc105b7c99284a3b5aa35bd515d674
Cristian <79643077+krisztian-Z@users.noreply.github.com>
2025-06-18 20:50:42 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -8,18 +8,25 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react_firebase_blog">
       <App />
     </BrowserRouter>
-    <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </React.StrictMode>
 );
-
+//test
 
 reportWebVitals();
-
-
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react'
 const NotFound = () => {
   return (
     <div>
+    
       <img src={`${process.env.PUBLIC_URL}/images/notFound.jpg`} alt="Page not found" />
+
 
     </div>
   )
